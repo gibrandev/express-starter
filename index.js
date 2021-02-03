846faa22ab180cd5f931781ca5de4d704845a43c
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-03 13:41:59 +0700

    Use libraries

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 require('dotenv').config();
 const port = process.env.APP_PORT || 3000;
 var Router = require('./routers/api');
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/', Router);
 
