8773a54d7f460393fb9c26045576265ebb7a6a6b
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-03 14:38:24 +0700

    Add socket io

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
+const http = require('http');
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
 const port = process.env.APP_PORT || 3000;
 const Router = require('./routers/api');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const consola = require('consola');
+const io = require('socket.io')(server);
 
+// Listen port
+server.listen(port, () => {
+    consola.success(`Listening at http://0.0.0.0:${port}`);
+});
+
+// Use libraries
 app.engine('pug', require('pug').__express);
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -14,8 +23,24 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Init route
 app.use('/', Router);
 
-app.listen(port, () => {
-    consola.success(`Listening at http://0.0.0.0:${port}`);
+// Socket.io
+io.on('connection', function (socket) {
+    console.log("Connected succesfully to the socket ...");
+
+    var news = [
+        { title: 'The cure of the Sadness is to play Videogames',date:'04.10.2016'},
+        { title: 'Batman saves Racoon City, the Joker is infected once again',date:'05.10.2016'},
+        { title: "Deadpool doesn't want to do a third part of the franchise",date:'05.10.2016'},
+        { title: 'Quicksilver demand Warner Bros. due to plagiarism with Speedy Gonzales',date:'04.10.2016'},
+    ];
+
+    // Send news on the socket
+    socket.emit('news', news);
+
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
