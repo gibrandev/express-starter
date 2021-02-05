c7b3262ec10c4adf69b3f91d27081acf229f70e5
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-05 22:11:40 +0700

    Add JWT_SECRET

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ rotator.register('./storage/logs/info.log', {
 */
 io.use((socket, next) => {
     if (socket.handshake.query && socket.handshake.query.token){
-        jwt.verify(socket.handshake.query.token, `your-256-bit-secret`, function(err, decoded) {
+        jwt.verify(socket.handshake.query.token, process.env.JWT_SECRET || '', function(err, decoded) {
         if(err) return next(new Error('Authentication error'));
             socket.decoded = decoded;
             next();
