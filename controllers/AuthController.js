7a66b16b268c096724adb718229999525793b13a
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-15 15:51:13 +0700

    Update jwt

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 exports.login = function(req, res) {
+    var token = jwt.sign({ username: 'gibrandev' }, process.env.JWT_SECRET || '');
     return res.status(200).json({
-        message: 'Login'
+        message: 'Login',
+        token: token
     });
 };
 
--- middlewares/UserMiddleware.js
+++ middlewares/UserMiddleware.js
@@ -1,7 +1,21 @@
+var jwt = require('jsonwebtoken');
 var UserMiddleware = async function (req, res, next) {
-    if(req.query.login) {
-        res.user = 'Gibran Dimasagung'
-        next()
+    var token = req.query.token || req.headers.authorization;
+    if(token) {
+        var bearer = token.replace(/ .*/, '');
+        if(bearer === 'Bearer' || bearer === 'bearer') {
+            token = token.replace('Bearer ','');
+        }
+        jwt.verify(token, process.env.JWT_SECRET || '', function(err, decoded) {
+            if(err) {
+                return res.status(401).json({
+                    message: 'Invalid token'
+                });
+            }
+            // query get user
+            res.user = decoded.username
+            next()
+        });
     } else {
         return res.status(401).json({
             message: 'Invalid token'
