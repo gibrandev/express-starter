4a89a93ff060fbb8b1e332485efe90ec4c9cefcd
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-17 11:04:28 +0700

    Fix middleware

--- middlewares/UserMiddleware.js
+++ middlewares/UserMiddleware.js
@@ -2,9 +2,12 @@ var jwt = require('jsonwebtoken');
 var UserMiddleware = async function (req, res, next) {
     var token = req.query.token || req.headers.authorization;
     if(token) {
-        var bearer = token.replace(/ .*/, '');
-        if(bearer === 'Bearer' || bearer === 'bearer') {
-            token = token.replace('Bearer ','');
+        var bearer = token.includes("Bearer") || token.includes("bearer");
+        if(bearer) {
+            var bearer = token.replace(/ .*/, '');
+            if(bearer === 'Bearer' || bearer === 'bearer') {
+                token = token.replace('Bearer ','');
+            }
         }
         jwt.verify(token, process.env.JWT_SECRET || '', function(err, decoded) {
             if(err) {
