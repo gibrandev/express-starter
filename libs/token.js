e482d7cb149ede24adc8bfe0c1c94a235cec18af
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-18 13:29:48 +0700

    Add jwt token checker

--- libs/token.js
+++ libs/token.js
@@ -1,6 +1,26 @@
 require('dotenv').config();
+const { v4: uuidv4 } = require('uuid');
+const redis = require('./redis')
 const jwt = require('jsonwebtoken');
-exports.generator = async function (req, res, next) {
+exports.generator = async (req, res, next) => {
     var host = req.get('host');
-    return jwt.sign({ iss: host, sub: 'gibrandev', type: 'user' }, process.env.JWT_SECRET || '');
+    var IdToken = uuidv4();
+    var token = jwt.sign({ iss: host, sub: 'gibrandev', type: 'user', jti: IdToken }, process.env.JWT_SECRET || '');
+    redis.set(`${process.env.REDIS_DB_NAME || 'Express'}:jwt:token:${IdToken}`, token);
+    return token;
 };
+
+exports.check = async (key) => {
+    return new Promise((resolve, reject) => {
+        redis.get(`${process.env.REDIS_DB_NAME || 'Express'}:jwt:token:${key}`, (e, data) => {
+            if(e){
+                reject(false);
+            }
+            if(data != null) {
+                resolve(true);
+            } else {
+                resolve(false)
+            }
+        });
+    });
+}
--- middlewares/UserMiddleware.js
+++ middlewares/UserMiddleware.js
@@ -1,4 +1,5 @@
 var jwt = require('jsonwebtoken');
+const JwtToken = require('../libs/token')
 var UserMiddleware = async function (req, res, next) {
     var token = req.query.token || req.headers.authorization;
     if(token) {
@@ -8,14 +9,22 @@ var UserMiddleware = async function (req, res, next) {
             var reg = new RegExp('bearer', 'gi');
             token = token.replace(reg, '').trim();
         }
-        jwt.verify(token, process.env.JWT_SECRET || '', function(err, decoded) {
+        // Verify
+        jwt.verify(token, process.env.JWT_SECRET || '', async (err, decoded) => {
             if(err) {
                 return res.status(401).json({
                     message: 'Invalid token'
                 });
             }
+            // Check token exist or not
+            var checkToken = await JwtToken.check(decoded.jti);
+            if(checkToken === false) {
+                return res.status(401).json({
+                    message: 'Invalid token'
+                });
+            }
             // query get user
-            res.user = decoded.username
+            res.user = decoded.sub
             next()
         });
     } else {
