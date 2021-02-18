9d239096c7739b6488ca6018ffa72ab97c2c5618
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-18 13:40:53 +0700

    Add redis options

--- libs/redis.js
+++ libs/redis.js
@@ -1,2 +1,10 @@
+require('dotenv').config();
 const redis = require("redis");
-module.exports = redis.createClient();
+const options = {
+    host: process.env.REDIS_DB_HOST || '127.0.0.1',
+    port: process.env.REDIS_DB_PORT || 6379,
+    auth_pass: process.env.REDIS_DB_PASSWORD || null,
+    db: 1,
+    prefix: process.env.REDIS_DB_PREFIX || 'Express'
+}
+module.exports = redis.createClient(options);
--- libs/token.js
+++ libs/token.js
@@ -6,13 +6,13 @@ exports.generator = async (req, res, next) => {
     var host = req.get('host');
     var IdToken = uuidv4();
     var token = jwt.sign({ iss: host, sub: 'gibrandev', type: 'user', jti: IdToken }, process.env.JWT_SECRET || '');
-    redis.set(`${process.env.REDIS_DB_NAME || 'Express'}:jwt:token:${IdToken}`, token);
+    redis.set(`jwt:token:${IdToken}`, token);
     return token;
 };
 
 exports.check = async (key) => {
     return new Promise((resolve, reject) => {
-        redis.get(`${process.env.REDIS_DB_NAME || 'Express'}:jwt:token:${key}`, (e, data) => {
+        redis.get(`jwt:token:${key}`, (e, data) => {
             if(e){
                 reject(false);
             }
