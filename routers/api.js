0a65c8ed6cf85e06b127fb74e1f49df6cc3a1e41
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-18 15:56:11 +0700

    Change middleware

--- routers/api.js
+++ routers/api.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var v1 = require('require-all')(process.cwd() + '/controllers/v1');
-var Middleware = require('../middlewares/UserMiddleware');
+var UserMiddleware = require('../middlewares/UserMiddleware');
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
 router.get('/login', v1.AuthController.login);
-router.get('/me', Middleware.auth, v1.AuthController.me);
-router.get('/logout', Middleware.auth, v1.AuthController.logout);
+router.get('/me', UserMiddleware.auth, v1.AuthController.me);
+router.get('/logout', UserMiddleware.auth, v1.AuthController.logout);
 
 module.exports = router;
