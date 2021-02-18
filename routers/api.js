a848bcbf215b7cc0ed598401c163f64de6a8d249
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-18 15:40:43 +0700

    Add required all controller

--- routers/api.js
+++ routers/api.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var AuthController = require('../controllers/AuthController');
+var v1 = require('require-all')(process.cwd() + '/controllers/v1');
 var UserMiddleware = require('../middlewares/UserMiddleware');
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
-router.get('/login', AuthController.login);
-router.get('/me', UserMiddleware, AuthController.me);
-router.get('/logout', UserMiddleware, AuthController.logout);
+router.get('/login', v1.AuthController.login);
+router.get('/me', UserMiddleware, v1.AuthController.me);
+router.get('/logout', UserMiddleware, v1.AuthController.logout);
 
 module.exports = router;
