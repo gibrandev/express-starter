b95dde7a68d66b6997bc51db0dbfe2f2e038838b
Gibran Dimasagung <gibrandev@gmail.com>
2021-02-25 09:50:31 +0700

    Set max memory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     apps : [{
         name: 'express-backend',
         script: "./index.js",
-        max_memory_restart: "150M",
+        max_memory_restart: "512M",
         env: {
             NODE_ENV: "production",
         }
