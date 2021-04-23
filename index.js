eb11230033bc3f24e2bd7d827d4e033c5f77a001
Gibran Dimasagung <gibrandev@gmail.com>
2021-04-23 15:47:56 +0700

    Disable log disconnect

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ io.on('connection', (client) => {
         });
     });
     client.on('disconnecting', () => {
-        console.log(client); // the Set contains at least the socket ID
+        // console.log(client); // the Set contains at least the socket ID
     });
 });
 /*
