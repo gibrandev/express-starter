8cd55908a3592ed8d2bab1d29818b21c08b48981
Gibran Dimasagung <gibrandev@gmail.com>
2021-04-23 10:01:26 +0700

    Hide column password json response

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 'use strict';
 const { Model } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
+const PROTECTED_ATTRIBUTES = ['password']
+
 module.exports = (sequelize, DataTypes) => {
     class user extends Model {
         /**
@@ -8,6 +10,14 @@ module.exports = (sequelize, DataTypes) => {
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
+        toJSON () {
+            // hide protected fields
+            let attributes = Object.assign({}, this.get())
+            for (let a of PROTECTED_ATTRIBUTES) {
+                delete attributes[a]
+            }
+            return attributes
+        }
         static associate(models) {
             user.hasMany(models.token, {foreignKey: 'object_id', as: 'tokens'});
         }
