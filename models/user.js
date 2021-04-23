557c8e8c775ff8b3e124c4a44cadf74344c9605d
Gibran Dimasagung <gibrandev@gmail.com>
2021-04-23 09:40:50 +0700

    Fix relation error

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // define association here
+            user.hasMany(models.token, {foreignKey: 'object_id', as: 'tokens'});
         }
     }
     user.init({
